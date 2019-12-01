534986cd0e3e9c11458108a7bd22d1376873f97d
jdelacruz07 <jdelacruz0077@gmail.com>
2019-12-01 21:50:10 +0100

    Se agrea la prueba de Stock

--- test/com/floristeria/application/ControllerTest.java
+++ test/com/floristeria/application/ControllerTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.Test;
 
 import com.floristeria.domain.Entity;
+import com.floristeria.domain.Stock;
 import com.floristeria.persistence.Repository;
 
 public class ControllerTest {
@@ -15,13 +16,18 @@ public class ControllerTest {
 	public void test() throws Exception {
 		Controller controller = new Controller();
 		Repository repository = new Repository();
-		
 		controller.createDecoration(Entity.getCounter(), "Tabla  ", 30, true, 20);
 		controller.createDecoration(Entity.getCounter(), "Tabla  ", 30, false, 20);
+		controller.seeStock();
 		List<Entity> items = repository.getAllInventory();
 		int count = items.size();
 		assertEquals(2, count);
-		
+	}
+	
+	@Test
+	public void stockTest() {
+		Stock stock = new Stock();
+		stock.stock();
 	}
 
 }
